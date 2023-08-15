306322aa9f88ce62fa98723821444e5969263bc7
Flopy <f.hidalgoalarcon@gmail.com>
2023-08-15 18:46:01 -0400

    Se crea e implementa clase AdapterList.

---
+++ app/src/main/java/com/example/phonenew/presentation/AdapterList.kt
@@ -0,0 +1,35 @@
+package com.example.phonenew.presentation
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.example.phonenew.data.local.CellPhoneEntity
+import com.example.phonenew.databinding.ItemBinding
+
+class AdapterList : RecyclerView.Adapter<AdapterList.ViewHolder>() {
+    lateinit var itemBinding: ItemBinding
+    private val listOfCellPhones = mutableListOf<CellPhoneEntity>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AdapterList.ViewHolder {
+        itemBinding = ItemBinding.inflate(LayoutInflater.from(parent.context))
+        return ViewHolder(itemBinding)
+    }
+
+    override fun onBindViewHolder(holder: AdapterList.ViewHolder, position: Int) {
+        val cellPhone = listOfCellPhones[position]
+        holder.bind(cellPhone)
+    }
+
+    override fun getItemCount(): Int {
+        return listOfCellPhones.size
+    }
+
+    class ViewHolder(private val binding: ItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(cellPhone: CellPhoneEntity) {
+            binding.imageItem.load(cellPhone.image)
+            binding.tvNameItem.text = cellPhone.name
+            binding.tvPriceItem.text = cellPhone.price.toString()
+        }
+    }
+}
