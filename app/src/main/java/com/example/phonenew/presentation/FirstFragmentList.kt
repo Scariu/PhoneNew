3c5810359dbe547322cb2f28601e82f1b79eeab2
Flopy <f.hidalgoalarcon@gmail.com>
2023-08-15 18:22:44 -0400

    Se crea FirstFragmentList y diseño base de fragment_first_list.xml

---
+++ app/src/main/java/com/example/phonenew/presentation/FirstFragmentList.kt
@@ -0,0 +1,23 @@
+package com.example.phonenew.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import com.example.phonenew.databinding.FragmentFirstListBinding
+
+
+class FirstFragmentList : Fragment() {
+    lateinit var binding: FragmentFirstListBinding
+    private val viewModel: CellPhoneViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentFirstListBinding.inflate(layoutInflater)
+        return binding.root
+    }
+}
